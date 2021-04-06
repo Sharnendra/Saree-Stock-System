ad8285268a412233e1927481f4813de78d2465f7
Sharnendra <sharnendradey@gmail.com>
2021-04-06 20:53:38 +0530

    Starting

---
+++ src/main/java/stock_service/stock_67/App.java
@@ -0,0 +1,13 @@
+package stock_service.stock_67;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
